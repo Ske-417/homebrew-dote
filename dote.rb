eee100c6f9bb105bfb6a70b140b402841d1d2213
佐藤 蒼介 <kmc2520@kamiyama.ac.jp>
2025-10-28 11:03:18 +0900

    Update homepage URL in dote.rb

--- dote.rb
+++ dote.rb
@@ -1,6 +1,6 @@
 class Dote < Formula
   desc "dot e command"
-  homepage "https://github.com/kmc2400/homebrew-dote"
+  homepage "https://github.com/Ske-417/homebrew-dote"
   url "https://raw.githubusercontent.com/kmc2400/homebrew-dote/main/dote.c"
   sha256 "PUT_SHA256_OF_DOTE.C_HERE"
 
