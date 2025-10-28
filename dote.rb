227c3c0ec5fc47a18cafac395a3fb9d918e1e81a
佐藤 蒼介 <kmc2520@kamiyama.ac.jp>
2025-10-28 13:24:45 +0900

    Update Dote formula for new description and license

--- dote.rb
+++ dote.rb
@@ -1,12 +1,17 @@
 class Dote < Formula
-  desc "dot e command"
-  homepage "https://github.com/Ske-417/homebrew-dote"
-  url "https://raw.githubusercontent.com/kmc2400/homebrew-dote/main/dote.c"
-  sha256 "97b6cc9b526fafa5707660842c91c4ab96cf6be1966d67d1abd76a28be2fa2ea"
+  desc "Full-screen ANSI meteor shower animation (2 spaces = 1 pixel)"
+  homepage "https://github.com/Ske-417/homebrew-meteor"
+  url "https://raw.githubusercontent.com/Ske-417/homebrew-dote/main/dote.c"
+  sha256 "<後で差し替え>"
+  license "MIT"
   version "1.0.0"
 
   def install
     system ENV.cc, "dote.c", "-o", "dote"
     bin.install "dote"
+  end
+
+  test do
+    assert_match "dote", shell_output("#{bin}/dote --version")
   end
 end
