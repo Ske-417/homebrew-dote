a7513681ad22859673e23103eb284bc101d0bb22
佐藤 蒼介 <kmc2520@kamiyama.ac.jp>
2025-10-28 13:28:03 +0900

    version "1.0.0"

--- dote.rb
+++ dote.rb
@@ -2,7 +2,7 @@ class Dote < Formula
   desc "Full-screen ANSI meteor shower animation (2 spaces = 1 pixel)"
   homepage "https://github.com/Ske-417/homebrew-meteor"
   url "https://raw.githubusercontent.com/Ske-417/homebrew-dote/main/dote.c"
-  sha256 "<後で差し替え>"
+  sha256 "2dfd8b37383019075e4ab9baf70ffc9893659966900f6e055f9d6d830e4d496d"
   license "MIT"
   version "1.0.0"
 
