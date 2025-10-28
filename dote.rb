e05229980268a77c51dd9a990008b6a5796cd250
佐藤 蒼介 <kmc2520@kamiyama.ac.jp>
2025-10-28 11:46:20 +0900

    version "1.0.0"

--- dote.rb
+++ dote.rb
@@ -2,7 +2,7 @@ class Dote < Formula
   desc "dot e command"
   homepage "https://github.com/Ske-417/homebrew-dote"
   url "https://raw.githubusercontent.com/kmc2400/homebrew-dote/main/dote.c"
-  sha256 "407a6e76c5fb79e041b56936926f90625748cc63369ce31292092dcea29a6ab1"
+  sha256 "97b6cc9b526fafa5707660842c91c4ab96cf6be1966d67d1abd76a28be2fa2ea"
   version "1.0.0"
 
   def install
